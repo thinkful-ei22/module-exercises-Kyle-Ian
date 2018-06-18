ea487a81165af57a863b8f3b5797c4625d3a7453
Ian Beihl <ianbeihl@gmail.com>
2018-06-18 14:24:55 -0700

    added checkWinner

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -3,8 +3,20 @@
 
 const state = {
   board: [null, null, null, null, null, null, null, null, null],
-  xIsNext: false,
-  winPattern: null
+  xIsNext: true,
+  winPattern: null,
+  newGame: function(){
+    this.board = Array(9).fill(null);
+    this.xIsNext = true;
+    this.winPattern = null;
+  },
+  setMove: function(cellNo){
+    const cell = Math.abs(cellNo);
+    if(this.winPattern) return;
+    if(this.board[cell] !== null) return;
+    this.board[cell] = this.xIsNext ? 'X' : 'O';
+    this.xIsNext = !this.xIsNext;
+  }
 };
 
 // State modification functions
@@ -40,10 +52,47 @@ const renderBoard = function() {
   };
   
   let html = assembleRows();
-  console.log(html);
   $('.board').html(html);
 };
 
 // Event Listeners
 
-$(renderBoard);
+function handleOnCellClick(){
+  function onCellClick(event){
+    const cellId = $(event.target).closest('.cell').attr('id');
+    state.setMove(cellId);
+    renderBoard();
+  }
+  $('.board').on('click','.cell', onCellClick);
+}
+
+function handleOnNewGameClick(){
+  function onNewGameClick(){
+    state.newGame();
+    renderBoard();
+  }
+  $('#new-game').click(onNewGameClick);
+}
+
+function checkWinner(board){
+  const winPatterns = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [2,4,6], [0,4,8]];
+  for(let i = 0; i < winPatterns.length; i++){
+    const winPattern = winPatterns[i];
+    //prevent win with three nulls by checking first cell isnt null
+    //I DONT GET IT!!!
+    if(!board[winPattern[0]]) continue;
+    if(board[winPattern[0]] === board[winPattern[1]] && board[winPattern[1]] === board[winPattern[2]]){
+      return winPattern;
+    }
+  }
+    return null;
+}
+
+$(function main(){
+  renderBoard();
+  handleOnCellClick();
+  handleOnNewGameClick();
+})
+
+
+
