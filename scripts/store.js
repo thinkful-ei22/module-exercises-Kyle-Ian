659a57d20a856231900e8dd68ff5acdce07c7ac8
Ian Beihl <ianbeihl@gmail.com>
2018-06-18 10:00:59 -0700

    added delete, add, edit

--- scripts/store.js
+++ scripts/store.js
@@ -9,9 +9,45 @@ const store = (function() {
   ];
   let hideCheckedItems = false;
   let searchTerm = '';
+  const findById = function(id){
+    let foundItem = store.items.find(item => {
+      item.id === id;
+    })
+    return foundItem;
+  }
+  const addItem = function(name){
+    try{
+      Item.validateName(name);
+      let item = Item.create(name);
+      this.items.push(item);
+    }
+    catch(err){
+      console.log(`Cannot add ${err.message}`);
+    }
+  }
+  const findAndToggleChecked = function(id){
+    let item = this.findById(id);
+    item.checked = !item.checked;
+  }
+  const findAndUpdateName = function(id, newName){
+    try{
+      Item.validateName(newName);
+      let item = Item.findById(id);
+      item.name = newName;
+    }
+    catch(err){
+      console.log(`Cannot update name: ${err.message}`);
+    }
+  }
+  const findAndDelete = function(id){
+    const itemIndex = store.items.findIndex(itemId => {
+       return itemId === id;
+    });
+    this.items.splice(itemIndex, 1);
+  }
 
   return {
-    items, hideCheckedItems, searchTerm
+    items, hideCheckedItems, searchTerm, findById, addItem, findAndToggleChecked, findAndUpdateName, findAndDelete
   };
 
 }());
